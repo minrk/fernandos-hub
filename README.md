8f2f3eb90fb8ce4cff5f07eaf71de0aeb9523848
Min RK <benjaminrk@gmail.com>
2025-11-10 15:32:20 -0800

    add jupyterhub

--- README.md
+++ README.md
@@ -4,9 +4,36 @@ Deployed on a Mac in his office.
 
 ## Deployment notes
 
-- hub environment: /opt/conda/envs/hub
-- user environment: /opt/conda/envs/user
-- this repo: /srv/jupyterhub
-
+- this repo: /opt/jupyterhub
+- envs with pixi:
+  - hub environment: /opt/conda/.pixi/envs/hub
+  - user environment: /opt/conda/.pixi/envs/user
 
 launchdaemon docs: https://developer.apple.com/library/archive/documentation/MacOSX/Conceptual/BPSystemStartup/Chapters/CreatingLaunchdJobs.html
+
+Making a system user for traefik:
+
+[ref](https://serverfault.com/questions/182347/add-daemon-account-on-os-x)
+
+```
+username_=traefik
+uid_=405
+realname_="Traefik Daemon"
+
+dscl . -create /Groups/_$username_
+dscl . -create /Groups/_$username_ PrimaryGroupID $uid_
+dscl . -create /Groups/_$username_ RecordName _$username_ $username_
+dscl . -create /Groups/_$username_ RealName $realname_
+dscl . -create /Groups/_$username_ Password \*
+
+dscl . -create /Users/_$username_
+dscl . -create /Users/_$username_ NFSHomeDirectory /xpt/local/apache2/wsgi/api
+dscl . -create /Users/_$username_ Password \*
+dscl . -create /Users/_$username_ PrimaryGroupID $uid_
+dscl . -create /Users/_$username_ RealName $realname_
+dscl . -create /Users/_$username_ RecordName _$username_ $username_
+dscl . -create /Users/_$username_ UniqueID $uid_
+dscl . -create /Users/_$username_ UserShell /usr/bin/false
+dscl . -delete /Users/_$username_ PasswordPolicyOptions
+dscl . -delete /Users/_$username_ AuthenticationAuthority
+```
---
+++ jupyterhub/jupyterhub_config.py
@@ -0,0 +1,77 @@
+"""
+JupyterHub config for the littlest jupyterhub.
+"""
+
+from pathlib import Path
+from secrets import token_hex
+import stat
+
+from traitlets.log import get_logger
+
+log = get_logger()
+
+c = get_config()  # noqa
+
+install_path = Path("/opt/jupyterhub")
+jupyterhub_dir = install_path / "jupyterhub"
+secrets_dir = install_path / "secrets"
+if not secrets_dir.exists():
+    secrets_dir.mkdir(mode=stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP, exist_ok=True)
+
+mode = secrets_dir.stat().st_mode
+if mode & stat.S_IRWXO:
+    log.warning(f"secrets dir {secrets_dir} has world-access. Locking it down.")
+    secrets_dir.chmod(stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP)
+
+
+traefik_password_file = secrets_dir / "traefik-password"
+cookie_secret_file = secrets_dir / "cookie-secret"
+db_file = secrets_dir / "jupyterhub.sqlite"
+db_file = db_file
+
+c.TraefikProxy.traefik_api_username = "jupyterhub"
+
+for secret_file in (traefik_password_file, cookie_secret_file):
+    if not secret_file.exists():
+        log.info(f"Generating secret in {secret_file}")
+        with secret_file.open("w") as f:
+            f.write(token_hex(32))
+
+with cookie_secret_file.open() as f:
+    c.JupyterHub.cookie_secret = f.read().strip()
+
+with traefik_password_file.open() as f:
+    c.TraefikProxy.traefik_api_password = f.read().strip()
+
+c.JupyterHub.db_url = f"sqlite:///{db_file}"
+
+# leave users running when the Hub restarts
+c.JupyterHub.cleanup_servers = False
+
+# externally managed traefik
+c.TraefikProxy.should_start = False
+c.JupyterHub.proxy_class = "traefik_file"
+
+# where jupyterhub rules go
+c.TraefikFileProviderProxy.dynamic_config_file = str(
+    install_path / "traefik/dynamic/jupyterhub.toml"
+)
+
+# public URL of this Hub
+c.JupyterHub.public_url = "https://longs.stat.berkeley.edu"
+
+# allowed users (allow all?)
+# for now: allow any PAM user
+c.Authenticator.allow_all = True
+
+c.Spawner.cmd = [
+    "pixi",
+    "run",
+    "--manifest-path",
+    str(install_path),
+    "-e",
+    "user",
+    "jupyterhub-singleuser",
+]
+
+c.JupyterHub.log_level = 10
