5ea653109ab47ad3ff5bc16ce4e5c90b7cc3dd4c
Min RK <benjaminrk@gmail.com>
2026-04-27 13:55:29 -0700

    try to fix ollama not finding mlx
    (didn't work)

--- models/start-ollama.sh
+++ models/start-ollama.sh
@@ -7,8 +7,13 @@ export PYTHONUNBUFFERED=1
 export PATH=/opt/homebrew/bin:$PATH
 
 # launch ollama
+export OLLAMA_NO_CLOUD=1
 export OLLAMA_MODELS=/opt/ollama
+export OLLAMA_LIBRARY_PATH=/opt/jupyterhub/.pixi/envs/user/lib
 export OLLAMA_NUM_PARALLEL=4
+export OLLAMA_DEBUG=1
 # for private key
 export HOME=/opt/ollama
+
+env | sort
 exec pixi run --as-is --environment=user ollama serve
